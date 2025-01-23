e7a4919c814a5bf100955f8567793c42a1a651e6
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2025-01-23 20:57:46 +0800

    update: 完成卡片布局和数据

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const Homepage = async () => {
   await delay(1000);
-  return <>ProStore</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" />
+    </>
+  );
 };
 
 export default Homepage;
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,8 +6,6 @@ import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
