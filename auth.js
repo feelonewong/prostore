932995fad8da89fde8ce1a048447ad1836fcc095
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2025-01-26 15:00:02 +0800

    update: 权限验证

---
+++ auth.js
@@ -0,0 +1,62 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+export const config = {
+  page: {
+    signIn: "/sign-in",
+    signOut: "/sign-out",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: { email: credentials.email },
+        });
+
+        // check if user exists and if the password matches
+
+        if (user && user.password) {
+          const isMatch = compareSync(credentials.password, user.password);
+          //   if password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // If user does not exist or password does not match return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }) {
+      //     set the user Id from the token
+      session.user.id = token.sub;
+      // if there is an update, set the username
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+      return session;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- db/seed.ts
+++ db/seed.ts
@@ -3,8 +3,16 @@ import sampleData from "@/db/sample-data";
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
+  // 生成产品数据的种子文件
   await prisma.product.createMany({ data: sampleData.products });
+
+  // 生成用户表的种子文件
+  await prisma.user.createMany({ data: sampleData.users });
   console.log("Database seeded successfully.");
 }
 
